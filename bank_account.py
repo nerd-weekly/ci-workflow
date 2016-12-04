fcb94a7a64063d99264e9a75f834716decdbb6b2
Katie Jones <katie.jones@reactive-robotics.com>
2016-12-04 15:17:41 +0100

    Keep list of account balances

--- bank_account.py
+++ bank_account.py
@@ -1,10 +1,10 @@
 class BankAccount:
     """ Represents a *very simple* bank account with debits and credits."""
     def __init__(self, initial_balance):
-        self.balance = initial_balance
+        self.balance = [initial_balance]
 
     def add_withdrawal(self, amount):
-        self.balance -= amount
+        self.balance.append(self.balance[-1] - amount)
 
     def add_deposit(self, amount):
-        self.balance += amount
+        self.balance.append(self.balance[-1] + amount)
