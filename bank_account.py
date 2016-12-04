eee66ac79c0c68a4dde29e0ffecb2be5b2ee941d
Katie Jones <katie.jones@reactive-robotics.com>
2016-12-04 15:24:15 +0100

    Store list of balances in separate member variable 'balances'

--- bank_account.py
+++ bank_account.py
@@ -1,10 +1,13 @@
 class BankAccount:
     """ Represents a *very simple* bank account with debits and credits."""
     def __init__(self, initial_balance):
-        self.balance = [initial_balance]
+        self.balance = initial_balance
+        self.balances = [initial_balance]
 
     def add_withdrawal(self, amount):
-        self.balance.append(self.balance[-1] - amount)
+        self.balances.append(self.balance - amount)
+        self.balance = self.balances[-1]
 
     def add_deposit(self, amount):
-        self.balance.append(self.balance[-1] + amount)
+        self.balances.append(self.balance + amount)
+        self.balance = self.balances[-1]
